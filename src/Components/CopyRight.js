303292754cda61789ba00c08bf5e6867542dba51
Hajra Khatri <khatrihajra173@gmail.com>
2023-03-10 12:46:42 +0530

    Copyright JS file

---
+++ src/Components/CopyRight.js
@@ -0,0 +1,14 @@
+const CopyRight = () => {
+  return (
+    <>
+      {/* Copyright section in here */}
+      <div className="container-fluid p-2 bg-dark bg-gradient-100 text-center">
+        <h6 className="text-white p-2">
+          &copy;2023 caretSurf All Rights Reserved
+        </h6>
+      </div>
+    </>
+  );
+};
+
+export default CopyRight;
